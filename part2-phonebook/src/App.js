1d62d5a0d7ce63219425885580c7595e0b1b36c6
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-01-14 20:38:43 +0700

    done Step3

--- part2-phonebook/src/App.js
+++ part2-phonebook/src/App.js
@@ -5,9 +5,11 @@ import Person from './components/Person';
 
 const App = () => {
   const [people, setPeople] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleAddPerson = (event) => {
     event.preventDefault();
@@ -18,20 +20,27 @@ const App = () => {
     if (typeof people.find((person) => person.name === newName) !== 'undefined') {
       alert(`${newName} is already added in the Phonebook!`)
       setNewName('')
+      setNewNumber('')
     }
     else {
       const newPersonObj = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
   
       setPeople(people.concat(newPersonObj))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -44,6 +53,13 @@ const App = () => {
             value={newName}
             onChange={handleNameChange}
             placeholder='new person...'></input>
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+            placeholder='new phone number...'></input>
         </div>
         <div>
           <button type="submit">add</button>
