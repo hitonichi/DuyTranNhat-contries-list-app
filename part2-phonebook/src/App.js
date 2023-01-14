a7b327b7675ccf5b7cee6b1d9d5ef2f169ea6b40
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-01-14 20:17:03 +0700

    done Step1

---
+++ part2-phonebook/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+import Person from './components/Person';
+
+const App = () => {
+  const [people, setPeople] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    console.log('btn clicked', event.target);
+
+    const newPersonObj = {
+      name: newName
+    }
+
+    setPeople(people.concat(newPersonObj))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+            placeholder='new person...'></input>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {people.map(person => <Person key={person.name} person={person}></Person>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
